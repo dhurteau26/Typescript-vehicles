d66b073b9be328f0de1696281ac758eb8dc89145
dhurteau26 <dhurteau26@gmail.com>
2024-09-02 00:17:14 -0400

    made changes to Cli.ts

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -254,10 +254,24 @@ class Cli {
         },
       ])
       .then((answers) => {
+        const motorbike = new Motorbike(
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          []
+        );
         // TODO: Use the answers object to pass the required properties to the Motorbike constructor
+        this.vehicles.push(motorbike);
         // TODO: push the motorbike to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the motorbike
+        this.selectedVehicleVin = motorbike.vin;
         // TODO: perform actions on the motorbike
+        this.performActions();
+      
       });
   }
 
